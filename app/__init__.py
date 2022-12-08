4d034e5a9a963bc587a8f7b457e187d5fe00b19c
lucas_costa <lucas.costa@tron.com.br>
2022-12-08 07:48:56 -0300

    Criei as tabelas

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storege.db'
+db = SQLAlchemy(app) 
+
+
 
 
 from app.controllers import default
 
-
+ 
 
